ee926ae2c9ff231fd3d3349804a196054bb72b51
Patrick Daniel <patcdaniel@gmail.com>
2018-04-30 09:48:52 -0700

    Updated some of the structure. Needs testing on a Pi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tmp_demo',
+      version='0.1',
+      description='Open House Raspberry Pi Temperature demo',
+      url='http://github.com/physoce/OH_Temperature_Demo',
+      author='Patrick Daniel',
+      author_email='patcdaniel@gmail.com',
+      license='MIT',
+      packages=['tmp_demo'],
+      zip_safe=False)
